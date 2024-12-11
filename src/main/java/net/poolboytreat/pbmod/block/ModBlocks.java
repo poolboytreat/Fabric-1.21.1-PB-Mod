c6710a061da492d725a330ca4d26e42205622399
Erueti-Selwyn <eselwyn@students.mags.school.nz>
2024-12-12 00:14:29 +1300

    Basic Item and Blocks added (No Functionality)

--- src/main/java/net/poolboytreat/pbmod/block/ModBlocks.java
+++ src/main/java/net/poolboytreat/pbmod/block/ModBlocks.java
@@ -1,11 +1,9 @@
 package net.poolboytreat.pbmod.block;
 
-import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.sound.BlockSoundGroup;
--- src/main/java/net/poolboytreat/pbmod/item/ModItems.java
+++ src/main/java/net/poolboytreat/pbmod/item/ModItems.java
@@ -1,8 +1,6 @@
 package net.poolboytreat.pbmod.item;
 
-import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
